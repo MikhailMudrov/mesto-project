a64b7fd63382d3570331c7aaf867d3889d9ebb4e
Mikhail Mudrov <mudrov@nlstar.com>
2022-02-14 21:59:05 +0300

    upd: реализовать редактированте формы

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
 'use strict';
-const element = document.querySelector('.profile__edit-button')
-const popup = document.querySelector('.popup')
-const popupClose = document.querySelector('.popup__close')
+//Открытие закрытие попапа
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popupProfile = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close');
 
-function popupOpen(a) {
-  a.classList.toggle('popup_opened');
+function popupOpenClose(popupClass) {
+  popupClass.classList.toggle('popup_opened');
 };
 
-element.addEventListener('click', function () { popupOpen(popup) })
-popupClose.addEventListener('click', function () { popupOpen(popup) })
+profileEditButton.addEventListener('click', function () { popupOpenClose(popupProfile) });
+popupCloseButton.addEventListener('click', function () { popupOpenClose(popupProfile) });
+
+
+
+//редактирование формы
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('#name');
+const jobInput = document.querySelector('#about');
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  title.textContent = nameInput.value;
+  subtitle.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', function () { popupOpenClose(popupProfile) });
+
+//актуализация формы
+function actualizationForm() {
+  document.querySelector('#name').value = title.textContent;
+  document.querySelector('#about').value = subtitle.textContent;
+}
+
+profileEditButton.addEventListener('click', actualizationForm);
+
+
+
 
